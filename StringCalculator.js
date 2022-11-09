8ef20106cd0d0859dd7a4b3d5dc2c36ca55b67e0
Enrique Sanchez <enrique.sanchez@autozone.com>
2022-11-08 21:42:42 -0600

    Step 1 Solution

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,6 +1,18 @@
 let StringCalculator = class {
     sum(list) {
-        return 0;
+        if (list.length == 0) {
+            return 0;
+        } else {
+            const array = list.split(",");
+            let result = 0;
+
+            array.forEach(element => {
+                const number = parseInt(element);
+                result += number;
+            });
+
+            return result;
+        }
     }
 }
 
--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -13,3 +13,29 @@ test('Given "" When sum Then 0', () => {
     expect(actualOutput).toBe(expectedOutput);
 });
 
+test('Given "1" When sum Then 1', () => {
+    // GIVEN
+    const calculator = new StringCalculator();
+    const input = "1";
+
+    // WHEN
+    const actualOutput = calculator.sum(input);
+
+    //THEN
+    const expectedOutput = 1;
+    expect(actualOutput).toBe(expectedOutput);
+});
+
+test('Given "1,2" When sum Then 3', () => {
+    // GIVEN
+    const calculator = new StringCalculator();
+    const input = "1,2";
+
+    // WHEN
+    const actualOutput = calculator.sum(input);
+
+    //THEN
+    const expectedOutput = 3;
+    expect(actualOutput).toBe(expectedOutput);
+});
+
